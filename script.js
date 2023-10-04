a20fc6b464af98306045df188b0ba40b91004695
Quebvihn <bamedinatorres@gmail.com>
2023-10-04 18:20:41 -0400

    adding time to the header

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-var firstHour = 6
-var lasthour = 17
 
-var currentTime = 
+function updateTime() {
+  var today = moment();
+
+  $("#currentDay").text(today.format("dddd, MMMM Do YYYY, h:mm:ss"));
 
 
-$(function () {
-currentTime = dayjs().hour();
-buildGrid();
+}
 
-$('.btn').on('click', setData);
-});
+updateTime()
+
+
+var saveBttn = $(".save-icon");
+var containerEl = $(".container");
+var schedule9am = $("#hour-9");
+var schedule10am = $("#hour-10");
+var schedule11am = $("#hour-11");
+var schedule12pm = $("#hour-12");
+var schedule1pm = $("#hour-1");
+var schedule2pm = $("#hour-2");
+var schedule3pm = $("#hour-3");
+var schedule4pm = $("#hour-4");
+var schedule5pm = $("#hour-5");
